f9368f93125c76a986d07b3b510b69095fa56912
shrimp <amoy0226@gmail.com>
2017-04-02 16:54:21 +0800

    支持指定集数

--- youku.js
+++ youku.js
@@ -2,10 +2,14 @@ const superagent = require('superagent');
 const cheerio = require('cheerio');
 const shell = require('shelljs');
 
-//node youku.js http://v.youku.com/v_show/id_XMjY4MjA5MDIxMg==.html 人民的名义
+//node youku.js http://v.youku.com/v_show/id_XMjY4MjA5MDIxMg==.html 人民的名义 1 5
 
 const url = process.argv[2];
 const name = process.argv[3];
+let start = process.argv[4] || 1; //起始集数
+let end = process.argv[5] || 0;   //结束集数，为0表示全部
+
+if (start <= 0) start = 1;
 
 shell.exec('mkdir ' + name);
 
@@ -16,12 +20,17 @@ superagent.get(url).end(function (err, res) {
 
     const $ = cheerio.load(res.text);
     const items = $('.tvlists .items').children('.item');
+    if (end <= 0 || end > items.length) {
+        end = items.length;
+    }
     items.each(function (i, item) {
-        const href = $(item).children('a').first().attr('href');
-        if (href) {
-            const cmd = `cd ${name} && you-get http:${href}`;
-            console.log(cmd);
-            shell.exec(cmd);
+        if (i >= start - 1 && i < end) {
+            const href = $(item).children('a').first().attr('href');
+            if (href) {
+                const cmd = `cd ${name} && you-get http:${href}`;
+                console.log(cmd);
+                shell.exec(cmd);
+            }
         }
     });
 
